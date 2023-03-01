fbc052dcbc5c97818aec6dc47ac00f8954f5a216
YuliaPysarenko <y.pisarenko@ukr.net>
2023-03-01 17:05:46 +0300

    homework

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,22 +1,4 @@
-// Напиши скрипт который:
 
-// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
-// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>)
-//  и количество элементов в категории(всех вложенных в него < li >).
-// Для выполнения этой задачи нужно использовать метод forEach() и свойства навигации по DOM.
-
-// В результате, в консоли будут выведены такие сообщения.
-
-// Number of categories                ?????????????????????????????
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
 
 const categoriesEl = document.querySelector(`#categories`);
 const itemsEl = document.querySelectorAll(`.item`);
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,8 +1,8 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
 
 
 // Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает
